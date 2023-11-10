2ef0f1a4dba0c017d091d3e63a4d37dab3ed6b78
Edison Jeon <edisonthe2nd@gmail.com>
2023-11-10 14:49:41 -0800

    finished including stretch

--- index.js
+++ index.js
@@ -1,27 +1,31 @@
-//❗❗ PLEASE READ THE README file for project instructions, helpful resources, additional tasks and stretch problems, and more ❗❗ 
+//❗❗ PLEASE READ THE README file for project instructions, helpful resources, additional tasks and stretch problems, and more ❗❗
 
 // ⭐️ Example Challenge START ⭐️
 
 /**Example Task : processFirstItem()
  * This example shows how you might go about solving the rest of the tasks
- * 
+ *
  * Use the higher order function processFirstItem below to do the following:
  *  1. Receive an array of strings in a parameter
  *  2. Receive a callback function that takes a string as its argument in a parameter
- *  3. Return the result of invoking the callback function and passing in the FIRST 
+ *  3. Return the result of invoking the callback function and passing in the FIRST
  *     element in the array as the argument
- * 
+ *
  * The following code is demonstrating a way of completing this task
  * It returns the string `foofoo`
-*/
+ */
 
 function processFirstItem(stringList, callback) {
-  return callback(stringList[0])
+  return callback(stringList[0]);
 }
-console.log('example task:', processFirstItem(['foo','bar'],function(str){return str+str}));
+console.log(
+  "example task:",
+  processFirstItem(["foo", "bar"], function (str) {
+    return str + str;
+  })
+);
 
 // ⭐️ Example Challenge END ⭐️
-
 
 ///// M V P ///////
 
@@ -29,9 +33,11 @@ console.log('example task:', processFirstItem(['foo','bar'],function(str){return
   
   Study the code for counter1 and counter2, then answer the questions below.
   
-  1. What is the difference between counter1 and counter2?
+  1. What is the difference between counter1 and counter2? 
+  A: 1 is an overhead function, 2 is a callback function.
   
   2. Which of the two uses a closure? How can you tell?
+  A: 1 === function scoped && 2 === globally scoped.
   
   3. In what scenario would the counter1 code be preferable? In what scenario would 
      counter2 be better?  
@@ -41,8 +47,8 @@ console.log('example task:', processFirstItem(['foo','bar'],function(str){return
 function counterMaker() {
   let count = 0;
   return function counter() {
-   return count++;
-  }
+    return count++;
+  };
 }
 
 const counter1 = counterMaker();
@@ -54,7 +60,6 @@ function counter2() {
   return count++;
 }
 
-
 /* ⚾️⚾️⚾️ Task 2: inning() ⚾️⚾️⚾️
 Use the inning function below to do the following:
   1. Return a random whole number of points between 0 and 2 scored by one team in an inning
@@ -64,10 +69,15 @@ Use the inning function below to do the following:
 NOTE: This will be a callback function for the tasks below
 */
 
-function inning(/*Code Here*/){
-    /*Code Here*/
+function inning() {
+  const randomNumber = Math.floor(Math.random() * 3);
+  return randomNumber;
 }
 
+// console.log(inning());
+// for (let i = 0; i < 10; i++) {
+//   console.log(inning())
+// }
 
 /* ⚾️⚾️⚾️ Task 3: finalScore() ⚾️⚾️⚾️
 Use the finalScore function below to do the following:
@@ -81,12 +91,20 @@ Use the finalScore function below to do the following:
   "Home": 11,
   "Away": 5
 }
-*/ 
+*/
 
-function finalScore(/*Code Here*/){
-  /*Code Here*/
+function finalScore(cb, num) {
+  let homeScore = 0;
+  let awayScore = 0;
+  for (let i = 1; i < num + 1; i++) {
+    homeScore += cb();
+    awayScore += cb();
+  }
+  // console.log({ Home: homeScore, Away: awayScore });
+  return { Home: homeScore, Away: awayScore };
 }
 
+finalScore(inning, 9);
 
 /* ⚾️⚾️⚾️ Task 4: getInningScore() ⚾️⚾️⚾️
 Use the getInningScore() function below to do the following:
@@ -100,12 +118,9 @@ For example: invoking getInningScore(inning) might return this object:
 }
   */
 
-
-function getInningScore(/*Your Code Here */) {
-  /*Your Code Here */
-
+function getInningScore(cb) {
+  return { Home: cb(), Away: cb() };
 }
-
 
 /* STRETCH: ⚾️⚾️⚾️ Task 5: scoreboard() ⚾️⚾️⚾️
 Use the scoreboard function below to do the following:
@@ -147,17 +162,35 @@ Use the scoreboard function below to do the following:
   "This game will require extra innings: Away 10 - Home 10"
 ] */
 // NOTE: There is no test associated with this code; if your output matches the given example, consider it complete!
-function scoreboard(/* CODE HERE */) {
-  /* CODE HERE */
+function scoreboard(cbInningScore, cbRandomNumber, num) {
+  const finalArr = [];
+  let finalHome = 0;
+  let finalAway = 0;
+
+  for (let i = 1; i < num + 1; i++) {
+    const home = cbInningScore(cbRandomNumber)["Home"];
+    const away = cbInningScore(cbRandomNumber)["Away"];
+    finalHome += home;
+    finalAway += away;
+    finalArr.push(`Inning ${i}: Away ${away} - Home ${home}`);
+  }
+
+  if (finalHome === finalAway) {
+    finalArr.push(
+      `This game will require extra innings: Away ${finalHome} - Home ${finalAway}`
+    );
+  } else {
+    finalArr.push(`Final Score: Away ${finalAway} - Home ${finalHome}`);
+  }
+  return finalArr;
 }
 
-
-
+console.log(scoreboard(getInningScore, inning, 9));
 
 /* 🛑🛑🛑🛑🛑 Please do not modify anything below this line 🛑🛑🛑🛑🛑 */
-function foo(){
-  console.log('its working');
-  return 'bar';
+function foo() {
+  console.log("its working");
+  return "bar";
 }
 foo();
 module.exports = {
@@ -169,4 +202,4 @@ module.exports = {
   finalScore,
   getInningScore,
   scoreboard,
-}
+};
